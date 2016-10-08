c3955f5ac28635062e4fad38631bb39e75cbba8e
Alexander Treshchev <treshev.a.d@gmail.com>
2016-10-08 23:20:23 +0300

    DS_C4_W1_assign completed

--- plot1.R
+++ plot1.R
@@ -2,15 +2,10 @@
 ## Making Plots of Electric power consumption's data set
 ## UC Irvine Machine Learning Repository of Data: http://archive.ics.uci.edu/ml/
 
-## Plot 1
 data_path <- "../datasciencecoursera/data/household_power_consumption.txt"
 
-# 1. Initial data analysis
-initial <- read.table(data_path, nrows = 6, sep = ";", header = TRUE)
-head(initial)
-dataclasses <- sapply(initial, class)
-
-# 2. Reading data rows corresponding to 2007-02-01 - 2007-02-02 period
+# 1. Reading data rows corresponding to 2007-02-01 - 2007-02-02 period
+#    (optimization for extra large file)
 library(lubridate)
 initdate <- dmy_hms(paste(initial$Date[1], initial$Time[1])) # get Date & Time from first row of data
 startdate <- dmy_hm("01/02/2007 00:00")
@@ -20,11 +15,10 @@ startrow <- as.numeric(difftime(startdate, initdate, units = "mins")) # get sear
 stoprow <- as.numeric(difftime(stopdate, initdate, units = "mins"))
 
 data <- read.table(data_path, skip = (startrow + 1), nrows = (stoprow + 1 - startrow),
-                   sep = ";", header = FALSE, col.names = colnames(initial), colClasses = dataclasses) # read desired rows only
-head(data)
-tail(data)
+                   sep = ";", na = "?", header = FALSE, col.names = colnames(initial),
+                   colClasses = dataclasses) # read desired rows only
 
-# 3. Drawing & writing Plot1 - Hist (GAP)
+# 2. Drawing & writing Plot1 - Hist (GAP)
 png(filename = "plot1.png", height = 600, width = 750)
 hist(data$Global_active_power, col = "red", main = "Global Active Power",
      xlab = "Global Active Power (kilowatts)")
